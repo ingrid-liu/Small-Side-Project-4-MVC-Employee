df9898257436336544b773115bbb142a3e99730a
AnnieBox <todearannie@gmail.com>
2021-08-11 15:39:44 -0700

    dynamic verification code img

---
+++ src/com/payrollsys/controller/CodeServlet.java
@@ -0,0 +1,107 @@
+package com.payrollsys.controller;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * Loading the verification code: generate a dynamic verification img (Servlet) in login.jsp
+ * @author  'Ingrid' Xiaoying Liu & Ryan Mah
+ * @version 1.0
+ * @since   8/6/21
+ */
+
+@WebServlet("/codeServlet")
+public class CodeServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // super.service(req, resp);
+        /*
+        Generate random picture verification code
+        1. draw numbers
+        2. choose different color for the nums
+        3. change font if needed
+        Reference:https://developpaper.com/generate-random-picture-verification-code-in-java/
+        Java provides a drawing tool: BufferedImage
+         */
+
+        //use built-in package image.BufferedImage to create verification img
+        int width = 110, height = 30;
+        BufferedImage image = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+
+        //get the context of the img
+        Graphics g = image.getGraphics();
+
+        //set the background color (otherwise it is black)
+        g.setColor(getRandColor(200, 250));
+        g.fillRect(0, 0, width, height);
+
+        //set font
+        g.setFont(new Font("Arial", Font.PLAIN, 20));       //Font.PLAIN font style
+
+        //randomly generate 200 interference lines to make the verification img hard to detect
+        Random random = new Random();       // generate a random object
+        g.setColor(getRandColor(160, 200));     // set interference lines' color
+        for (int i = 0; i < 200; i++) {
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+            int xl = random.nextInt(12);
+            int yl = random.nextInt(12);
+            g.drawLine(x, y, x + xl, y + yl);   // two coordinates to draw one line
+        }
+
+        // randomly generate 6-digit verification code
+        String sRand = "";
+        for (int i = 0; i < 6; i++) {
+            String rand = String.valueOf(random.nextInt(10));
+            sRand += rand;
+            // show the code in the img
+            g.setColor(new Color(20 + random.nextInt(110), 20 + random
+                    .nextInt(110), 20 + random.nextInt(110)));
+            // todo Question: 调用函数出来的颜色相同，可能是因为种子太接近，所以只能直接生成
+            // The colors from the calling function are the same, maybe because the seeds are too close,
+            // so they can only be generated directly
+            g.drawString(rand, 13 * i + 6, 16);
+        }
+
+
+        //Graph object dispose (https://stackoverflow.com/questions/13911743/why-does-calling-dispose-on-graphics-object-cause-jpanel-to-not-render-any-com)
+        g.dispose();
+        try {
+            ImageIO.write(image, "JPEG", resp.getOutputStream());
+        } catch (Exception e) {
+            System.out.println("An error occurred:" + e.toString());
+        }
+        //save the verification code to Session (submit with the ID/PSW and do the match work in next step)
+        req.getSession().setAttribute("randStr", sRand);
+
+    }
+
+
+
+    /**
+     * The getRandColor function randomly gets a color.
+     * @param fc an integer to define the color space value
+     * @param bc an integer to define the color space value
+     * @return a Color object
+     */
+    private Color getRandColor(int fc, int bc) {
+        Random random = new Random();
+        if (fc > 255)
+            fc = 255;
+        if (bc > 255)
+            bc = 255;
+        int r = fc + random.nextInt(bc - fc);
+        int g = fc + random.nextInt(bc - fc);
+        int b = fc + random.nextInt(bc - fc);
+        return new Color(r, g, b);
+    }
+}
--- web/login.jsp
+++ web/login.jsp
@@ -1,9 +1,10 @@
 <%--
   Created by IntelliJ IDEA.
-  User: sparkle
-  Date: 8/9/21
+  User: ingridliu
+  Date: 8/5/21
   Time: 11:22 PM
   To change this template use File | Settings | File Templates.
+
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -23,6 +24,20 @@
         });
     </script>
 
+    <%-- js code statement   --%>
+    <script type="text/javascript">
+    <%-- add 'single click event' to the verification code img--%>
+        $(function () {
+            $('#codeImg').click(function () {
+                // get the codImg object
+                var img=$('#codeImg');
+                // modify scr attribution value, but not the uri value
+                img.attr('src', img.attr('src')+'?'+Math.random())      //original value:img.attr("src")
+
+            })
+        })
+    </script>
+
 </head>
 
 <body style="background-color:#1c77ac; background-image:url(images/light.png); background-repeat:no-repeat; background-position:center top; overflow:hidden;">
@@ -36,7 +51,7 @@
 
 
 <div class="logintop">
-    <span>Welcome to Payroll System</span>
+    <span><b>Payroll system dev by Ingrid and Ryan</b></span>
     <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">Help</a></li>
@@ -54,7 +69,13 @@
             <li><input name="" type="text" class="loginuser" value="admin" onclick="JavaScript:this.value=''"/></li>
             <li><input name="" type="text" class="loginpwd" value="password" onclick="JavaScript:this.value=''"/></li>
             <li class="yzm">
-                <span><input name="" type="text" value="verification" onclick="JavaScript:this.value=''"/></span><cite>X3D5S</cite>
+                <%--  loading the verification code: generate a dynamic verfication img (Servlet)--%>
+                <%--  click image->change veri code: browser will automatically generated new tag of the resource(so does url) --%>
+                <span><input name="" type="text" value="verification" onclick="JavaScript:this.value=''"/></span>
+                    <cite>
+                        <img id="codeImg" style="cursor:pointer" src="codeServlet" alt="" width="115px" height="46px">       <%--  call servlet to get the new code --%>
+                        <%-- in head use jquery to set the single click event; stype: use css to change cursor as a hand   --%>
+                    </cite>
             </li>
             <li><input name="" type="button" class="loginbtn" value="Login"  onclick="javascript:window.location='main.html'"  /><label><input name="" type="checkbox" value="" checked="checked" />Remember me</label><label><a href="#">Forget Password？</a></label></li>
         </ul>
