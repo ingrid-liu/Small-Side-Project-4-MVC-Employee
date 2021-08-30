489cbbade26fe6ca958a304f2a405d153da8bad1
AnnieBox <todearannie@gmail.com>
2021-08-29 18:19:57 -0700

    init spring boot

---
+++ employeemanagement/src/main/java/com/project/employeemanagement/EmployeemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagementApplication.class, args);
+	}
+
+}
---
+++ employeemanagement/src/main/java/com/project/employeemanagement/model/Employee.java
@@ -0,0 +1,100 @@
+package com.project.employeemanagement.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imagUrl;         //can be File also if needed;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;    //random code
+
+    public Employee() {
+    }
+
+    public Employee(String name, String email, String jobTitle, String phone, String imagUrl, String employeeCode) {
+        this.name = name;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.phone = phone;
+        this.imagUrl = imagUrl;
+        this.employeeCode = employeeCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImagUrl() {
+        return imagUrl;
+    }
+
+    public void setImagUrl(String imagUrl) {
+        this.imagUrl = imagUrl;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imagUrl='" + imagUrl + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
+}
