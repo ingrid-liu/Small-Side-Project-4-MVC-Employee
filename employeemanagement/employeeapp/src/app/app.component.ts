debee06e4e2ef07637a8d14a022a416a42c2e6bd
AnnieBox <todearannie@gmail.com>
2021-08-29 22:57:50 -0700

    Angular Component

--- employeemanagement/employeeapp/src/app/app.component.ts
+++ employeemanagement/employeeapp/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from "./employee";
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeeapp';
+export class AppComponent implements OnInit {
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
 }
