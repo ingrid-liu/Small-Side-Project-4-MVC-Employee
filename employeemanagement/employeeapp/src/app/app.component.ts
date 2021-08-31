07c1560ecb6174a4c55a3384adfca9682c1417cb
AnnieBox <todearannie@gmail.com>
2021-08-31 00:06:18 -0700

    Search Functionality

--- employeemanagement/employeeapp/src/app/app.component.ts
+++ employeemanagement/employeeapp/src/app/app.component.ts
@@ -71,6 +71,23 @@ export class AppComponent implements OnInit {
     );
   }
 
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.indexOf(key) !== -1
+      || employee.email.indexOf(key) !== -1
+      || employee.phone.indexOf(key) !== -1
+      || employee.jobTitle.indexOf(key) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
+
 
   public onOpenModal(employee: Employee, mode: string): void {
     const container = document.getElementById('main-container');
